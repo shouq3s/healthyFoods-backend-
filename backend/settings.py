7d47de550edad3e970563bf0ec7e0791cc05c1b1
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-04-29 21:41:17 +0300

    add healthy foods models

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'healthyfoods',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class HealthyFoods(models.Model):
+    foodName = models.CharField(max_length=250)
+    Ingredients = models.CharField(max_length=555)
+    calories = models.IntegerField()
+    Protien = models.IntegerField()
+    Fiber = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.foodName} - {self.calories}'
+
+
