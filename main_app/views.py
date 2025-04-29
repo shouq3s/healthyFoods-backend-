f80aec20b901b73f24daf4d784a8e257fa1abb58
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-04-29 22:20:13 +0300

    create foods list create view

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import HealthyFoods
+from .serializers import HealthyFoodsSerializers
 # Create your views here.
+class FoodsListCreateView(APIView):
+    def get(self,request):
+        Foods = HealthyFoods.objects.all()
+        serializer = HealthyFoodsSerializers(Foods, many=True)
+        return Response(serializer.data,status=200) 
