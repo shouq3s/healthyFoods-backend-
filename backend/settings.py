03fa0ba8a3010968dfd0d867a45a3b6232a834f7
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-04-30 17:58:51 +0300

    add a foods request to Foods List Create View

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'main_app',
     'django.contrib.admin',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,9 @@ class FoodsListCreateView(APIView):
         Foods = HealthyFoods.objects.all()
         serializer = HealthyFoodsSerializers(Foods, many=True)
         return Response(serializer.data,status=200) 
+    def post(self,request):
+        serializer = HealthyFoodsSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
