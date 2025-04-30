65c089fdcd3158ca35264a69fd96c8ca40a29062
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-04-30 23:31:33 +0300

    change in the models

--- main_app/models.py
+++ main_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class HealthyFoods(models.Model):
     foodName = models.CharField(max_length=250)
-    Ingredients = models.CharField(max_length=555)
     calories = models.IntegerField()
     Protien = models.IntegerField()
     Fiber = models.IntegerField()
+    Ingredients = models.TextField(max_length=555)
 
     def __str__(self):
-        return f'{self.foodName} - {self.calories}'
+        return f'{self.foodName} - {self.calories} Cals'
 
 
