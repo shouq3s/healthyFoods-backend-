c37be49ecd96249f21d6814c36d7e1b4ac4628dc
shouqAlbalawi <shouqalbalawi65@gmail.com>
2025-05-04 11:15:04 +0300

    delete comments

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,12 +6,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('healthyfoods/', FoodsListCreateView.as_view() , name ='create-foods-list'),
     path('healthyfoods/<int:pk>/',FoodsDetailsView.as_view(),name='foods_detail' ),  
-    path('collections/', CollectionList.as_view()),
+    path('collections/', CollectionList.as_view(), name='collection'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('healthyfoods/<int:foods_id>/add-collection/<int:collection_id>/', add_collection_to_foods, name='add-collection-to-foods'),
 
-  #  path('healthyfoods/<int:foods_id>/add-collection/<int:collection_id>', add_toy_to_cat, name='api_add_toy_to_cat'),
 
 ]
